4875185fc2be2c72afc94179e45ff75538bf7621
Mathieu Villaume <mathieu.villaume@grenoble-inp.org>
2026-01-01 20:01:13 +0100

    stack allocated

--- tests.c
+++ tests.c
@@ -3,45 +3,47 @@
 #include "vec.h"
 
 int main() {
-  Vec *vec;
+  Vec vec;
   assert(new_vec(&vec) == VEC_OK);
-  assert(vec->size == 0);
-  assert(vec->capacity == 8);
+  assert(vec.size == 0);
+  assert(vec.capacity == 8);
 
-  assert(vec_set(vec, 0, 1) == VEC_INDEX_OUT_OF_BOUNDS);
+  assert(vec_set(&vec, 0, 1) == VEC_INDEX_OUT_OF_BOUNDS);
 
-  assert(vec_push(vec, 1) == VEC_OK);
-  assert(vec->size == 1);
+  assert(vec_push(&vec, 1) == VEC_OK);
+  assert(vec.size == 1);
   assert(vec_get(vec, 0).val == 1);
 
-  for (size_t i = 1; i < vec->capacity; i++) {
-    assert(vec_push(vec, 2) == VEC_OK);
+  for (size_t i = 1; i < vec.capacity; i++) {
+    assert(vec_push(&vec, 2) == VEC_OK);
   }
 
   vec_print_debug(vec);
 
-  assert(vec_push(vec, 0) == VEC_OK);
-  assert(vec->capacity == 16);
-  assert(vec->size = 8);
+  assert(vec_push(&vec, 0) == VEC_OK);
+  assert(vec.capacity == 16);
+  assert(vec.size = 8);
 
   assert(vec_get(vec, 8).type == OPTION_NONE);
 
-  assert(vec_pop(vec, 0).type == OPTION_SOME);
-  assert(vec_pop_last(vec).type == OPTION_SOME);
-  assert(vec_pop(vec, 2).type == OPTION_SOME);
-  assert(vec->size == 5);
-  assert(vec_pop(vec, 6).type == OPTION_NONE);
+  assert(vec_pop(&vec, 0).type == OPTION_SOME);
+  assert(vec_pop_last(&vec).type == OPTION_SOME);
+  assert(vec_pop(&vec, 2).type == OPTION_SOME);
+  assert(vec.size == 5);
+  assert(vec_pop(&vec, 6).type == OPTION_NONE);
 
   vec_print_debug(vec);
 
   assert(vec_shrink(&vec) == VEC_OK);
-  assert(vec->capacity == vec->size);
+  assert(vec.capacity == vec.size);
   vec_print_debug(vec);
 
-  assert(vec_empty(vec) == VEC_OK);
-  assert(vec->size == 0);
+  assert(vec_empty(&vec) == VEC_OK);
+  assert(vec.size == 0);
 
-  vec_free(vec);
+  assert(vec_shrink(&vec) == VEC_EMPTY);
+
+  vec_free(&vec);
   printf("Tests done :)\n");
   return 0;
 }
--- vec.c
+++ vec.c
@@ -1,42 +1,38 @@
 #include "vec.h"
 
-vec_result new_vec_capacity(Vec **vec, size_t capacity) {
+vec_result new_vec_capacity(Vec *vec, size_t capacity) {
   if (capacity < 4)
     return VEC_ALLOC_ERR;
 
-  *vec = (Vec *)malloc(sizeof(Vec));
-  if (!(*vec))
-    return VEC_ALLOC_ERR;
-
-  (*vec)->capacity = capacity;
-  (*vec)->size = 0;
-  (*vec)->data = (u32 *)malloc(capacity * sizeof(u32));
+  vec->capacity = capacity;
+  vec->size = 0;
+  vec->data = (u32 *)malloc(capacity * sizeof(u32));
 
   return VEC_OK;
 }
 
-vec_result new_vec(Vec **vec) {
+vec_result new_vec(Vec *vec) {
   return new_vec_capacity(vec, 8);
 }
 
-vec_result vec_resize(Vec **vec) {
-  if (!(*vec))
+vec_result vec_resize(Vec *vec) {
+  if (!vec)
     return VEC_INVALID_PTR;
 
-  if ((*vec)->capacity > SIZE_MAX / RESIZE_MUL)
+  if (vec->capacity > SIZE_MAX / RESIZE_MUL)
     return VEC_ALLOC_ERR;
 
-  size_t new_capacity = (*vec)->capacity * RESIZE_MUL;
+  size_t new_capacity = vec->capacity * RESIZE_MUL;
 
   if (new_capacity > SIZE_MAX / sizeof(u32))
     return VEC_ALLOC_ERR;
 
-  u32 *tmp = (u32 *)realloc((*vec)->data, new_capacity * sizeof(u32));
+  u32 *tmp = (u32 *)realloc(vec->data, new_capacity * sizeof(u32));
   if (!tmp)
     return VEC_ALLOC_ERR;
 
-  (*vec)->capacity = new_capacity;
-  (*vec)->data = tmp;
+  vec->capacity = new_capacity;
+  vec->data = tmp;
 
   return VEC_OK;
 }
@@ -57,7 +53,7 @@ vec_result vec_set(Vec *vec, size_t idx, u32 val) {
      return VEC_INVALID_PTR;
 
    if (vec->size >= vec->capacity) {
-     vec_result res = vec_resize(&vec);
+     vec_result res = vec_resize(vec);
      if (res != VEC_OK)
        return res;
    }
@@ -71,19 +67,18 @@ vec_result vec_set(Vec *vec, size_t idx, u32 val) {
 
 void vec_free(Vec *vec) {
   free(vec->data);
-  free(vec);
 }
 
-Option vec_get(const Vec *vec, size_t idx) {
-   if (!vec || idx >= vec->size)
+Option vec_get(Vec vec, size_t idx) {
+   if (idx >= vec.size)
      return option_none();
 
-   u32 val = vec->data[idx];
+   u32 val = vec.data[idx];
    return option_some(val);
 }
 
 Option vec_pop(Vec *vec, size_t idx) {
-  Option ret = vec_get(vec, idx);
+  Option ret = vec_get(*vec, idx);
   if (ret.type == OPTION_NONE)
     return ret;
 
@@ -100,50 +95,45 @@ Option vec_pop_last(Vec *vec) {
   return vec_pop(vec, vec->size-1);
 }
 
-void vec_print(const Vec *vec) {
-  if (!vec || !(vec->data))
+void vec_print(Vec vec) {
+  if (!(vec.data))
     return;
   
   printf("[");
-  for (size_t idx = 0; idx < vec->size; idx++) {
-    printf("%d", vec->data[idx]);
-    if (idx != vec->size-1)
+  for (size_t idx = 0; idx < vec.size; idx++) {
+    printf("%d", vec.data[idx]);
+    if (idx != vec.size-1)
       printf(", ");
   }
   printf("]\n");
 }
 
-void vec_print_debug(const Vec *vec) {
-  if (!vec) {
-    printf("Invalid ptr\n");
-    return;
-  }
-
-  if (!(vec->data)) {
+void vec_print_debug(Vec vec) {
+  if (!(vec.data)) {
     printf("Data ptr invalid\n");
     return;
   }
 
-  printf("Capacity: %ld\n", vec->capacity);
-  printf("Size: %ld\n", vec->size);
+  printf("Capacity: %ld\n", vec.capacity);
+  printf("Size: %ld\n", vec.size);
   vec_print(vec);
 }
 
-vec_result vec_shrink(Vec **vec) {
-  if (!(*vec))
+vec_result vec_shrink(Vec *vec) {
+  if (!vec)
     return VEC_INVALID_PTR;
 
-  if ((*vec)->size == 0)
+  if (vec->size == 0)
     return VEC_EMPTY;
 
-  size_t new_capacity = (*vec)->size;
+  size_t new_capacity = vec->size;
 
-  u32 *tmp = (u32 *)realloc((*vec)->data, new_capacity * sizeof(u32));
+  u32 *tmp = (u32 *)realloc(vec->data, new_capacity * sizeof(u32));
   if (!tmp)
     return VEC_ALLOC_ERR;
 
-  (*vec)->capacity = new_capacity;
-  (*vec)->data = tmp;
+  vec->capacity = new_capacity;
+  vec->data = tmp;
 
   return VEC_OK;
 }
